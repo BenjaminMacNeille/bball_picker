f56e67d8d09accccf55c61a4301cac6664e88ac8
Benjamin MacNeille <bcmacneille@gmail.com>
2022-10-17 19:45:15 -0400

    updated picker

--- bball_analysis.py
+++ bball_analysis.py
@@ -58,19 +58,18 @@ class bb_picker:
         else:
             self.players_df = self.df
         
-        top_150_df = self.players_df.head(150)
+        top_150_df = self.players_df.head(120)
         def _weighted_avg(total_col, percentage_col):
             top_150_df[total_col+"_weighted"] = top_150_df[total_col]*top_150_df[percentage_col]
             avg = top_150_df[total_col+"_weighted"].sum()/top_150_df[total_col].sum()
-            print(avg)
             return avg
-        fg_avg = _weighted_avg("FGA", "FG%")
-        ft_avg = _weighted_avg("FTA", "FT%")
+        self.fg_avg = _weighted_avg("FGA", "FG%")
+        self.ft_avg = _weighted_avg("FTA", "FT%")
 
         self.avg_dict = {}
 
-        self.avg_dict["FG%"] = fg_avg
-        self.avg_dict["FT%"] = ft_avg
+        self.avg_dict["FG%"] = self.fg_avg
+        self.avg_dict["FT%"] = self.ft_avg
 
         for col in self.totals_cols:
 
@@ -80,67 +79,141 @@ class bb_picker:
     def picker(self):
 
         players_df = self.players_df
-        print(players_df.head())
+
+        players_df = players_df[['PLAYER', 'PTS', 'REB', 'AST', 
+                                'BLK', 'STL', 'FG%', 'FT%',
+                                '3PM', 'GP', 'FGA', 'FTA']]
+        print(players_df.head(50))
+        
         
         percent_dict = {"FG%":"FGA", 
                         "FT%": "FTA"}
 
-        for col in self.cols:
+        for col in self.totals_cols:
 
             players_df[col] = pd.to_numeric(players_df[col], errors='coerce')
 
-        drafted_players = players_df.head(150)
+        drafted_players = players_df.head(120)
         #print(list(drafted_players))
         
         averaged_list = []
         
-        for col in self.cols:
+        for perc_col, att_col in percent_dict.items():
+            players_df[att_col] = round(players_df[att_col], 3)
+
+        for col in self.totals_cols:
+            if col != "GP":
+                players_df[col] = round(players_df[col]/players_df["GP"],3)
+                drafted_players[col] = drafted_players[col]/drafted_players["GP"]
 
             averaged_list.append(drafted_players[col].mean())
-            print(col, ": ", drafted_players[col].mean())
+            print(col, "NBA avg: ", round(drafted_players[col].mean(), 3))
 
-        #print(averaged_list)
+        print("FG%",  "NBA avg: ", round(self.fg_avg, 3))
+        print("FT%",  "NBA avg: ", round(self.ft_avg, 3))
 
         counter = 0
 
         col_subset = ["PLAYER",
-                        "3PM",
                         'PTS', 
                         'REB', 
                         'AST', 
                         'STL', 
                         'BLK',
+                        "3PM",
+                        "FGA",
+                        "FG%",
+                        "FTA",
+                        "FT%",
                         "GP",
                         ]
-
+        players_df = players_df[col_subset]
         drafted_players = drafted_players[col_subset]
 
         addative_df = pd.DataFrame(columns=col_subset)
         
         while counter < 13:
+            print("\n")
+            drafted = input("Is this your draft pick? (y/n)")
 
-            player_taken = input("Which player did you draft?")
+            if drafted == "y":
+                
+                print(players_df.head(50))
+                print("\n")
+                for col in self.totals_cols:
+                    print(col," : ", round(addative_df[col].mean(), 3), "  NBA avg: ", round(drafted_players[col].mean(), 3))
 
-            player_list = players_df["PLAYER"].unique()
+                def weighted_avg(total_col, percentage_col):
+                    try:
+                        addative_df[total_col+"_weighted"] = addative_df[total_col]*addative_df[percentage_col]
+                        avg = round(addative_df[total_col+"_weighted"].sum()/addative_df[total_col].sum(),3)
+                        return avg
+                    except:
+                        return "NA"
+                
+                fg = weighted_avg("FGA","FG%")
+                ft = weighted_avg("FTA","FT%")
+                
+                print("FG%"," : ", fg,"  NBA avg: ", round(self.fg_avg, 3))
+                print("FT%"," : ", ft,"  NBA avg: ",round(self.ft_avg, 3))
 
-            if player_taken in player_list:
+                player_taken = input("Which player did you draft?")
 
-                drafted_player_df = players_df[players_df["PLAYER"] == player_taken]
+                player_list = players_df["PLAYER"].unique()
 
-                drafted_player_df = drafted_player_df[col_subset]
+                if player_taken in player_list:
 
-                addative_df = addative_df.append(drafted_player_df)
+                    drafted_player_df = players_df.loc[players_df["PLAYER"] == player_taken]
 
-                counter += 1
+                    drafted_player_df = drafted_player_df[col_subset]
 
-                for col, average in zip(self.cols, averaged_list):
-                        #print(col, addative_df[col].sum(), average,counter)
+                    addative_df = addative_df.append(drafted_player_df)
+                    
 
-                        print(col, 
-                            round((1+(addative_df[col].sum() - average*counter)/average)*100, 1)
-                            )
+                    counter += 1
+
+                    players_df = players_df[players_df["PLAYER"] != player_taken]
+                    print("\n")
+                    for col in self.totals_cols:
+                            
+                            print(col," : ", round(addative_df[col].mean(), 3),"  NBA avg: ", round(drafted_players[col].mean(), 3))
+                    
+                    def weighted_avg(total_col, percentage_col):
+                        try:
+                            addative_df[total_col+"_weighted"] = (addative_df[total_col]*addative_df[percentage_col])
+
+                            avg = addative_df.loc[total_col+"_weighted"].sum()/addative_df[total_col].sum()
+                            return avg
+                        except:
+                            return "NA"
+                
+                    fg = weighted_avg("FGA", "FG%")
+                    ft = weighted_avg("FTA", "FT%")
+                    
+                    print("FG%"," : ", fg,"  NBA avg: ", round(self.fg_avg, 3))
+                    print("FT%"," : ", ft,"  NBA avg: ",round(self.ft_avg, 3))
+
+                else:
+                    print("player not found, try again")
+                    continue
+            
+            elif drafted == "n":
+
+                #print(players_df.head(50))
+
+                player_taken = input("Which player was drafted?")
+
+                player_list = players_df["PLAYER"].unique()
+
+                if player_taken in player_list:
+                
+                    players_df = players_df[players_df["PLAYER"] != player_taken]
+                
+                else:
+                    print("player not found, try again")
+                    continue
             else:
-                print("player not found, try again")
+                print("invalid key, try again")
                 continue
 
 
@@ -154,4 +227,4 @@ if __name__ == "__main__":
         bp.join_dfs()
         bp.format_joined_df()
     bp.get_averages()
-    #bp.picker()
+    bp.picker()
